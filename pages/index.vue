f1462b93c1e539fd3dd04f2c0e587111540698ca
Azmat Ali <azmat1080@gmail.com>
2023-02-16 02:48:49 +0530

    login page design fixes

--- pages/index.vue
+++ pages/index.vue
@@ -2,12 +2,12 @@
 	<div>
 		<section class="bg-gray-50 dark:bg-gray-900">
 			<div
-				class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+				class="flex flex-col items-center justify-center px-6 py-8 mx-auto h-screen lg:py-0">
 				<div
 					class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
 					<div class="p-6 space-y-4 md:space-y-6 sm:p-8">
 						<h1
-							class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+							class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white mb-8">
 							Sign in to your account
 						</h1>
 						<form class="space-y-4 md:space-y-6" @submit.prevent="signIn">
@@ -18,6 +18,7 @@
 									>Your email</label
 								>
 								<input
+									@click="errMsg = null"
 									v-model="email"
 									type="email"
 									name="email"
@@ -33,51 +34,21 @@
 									>Password</label
 								>
 								<input
+									@click="errMsg = null"
 									v-model="password"
 									type="password"
 									name="password"
 									id="password"
 									placeholder="••••••••"
-									class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+									class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 mb-6"
 									required="" />
 							</div>
-							<p v-if="errMsg" class="text-red-500">{{ errMsg }}</p>
-							<div class="flex items-center justify-between">
-								<div class="flex items-start">
-									<div class="flex items-center h-5">
-										<input
-											id="remember"
-											aria-describedby="remember"
-											type="checkbox"
-											class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" />
-									</div>
-									<div class="ml-3 text-sm">
-										<label
-											for="remember"
-											class="text-gray-500 dark:text-gray-300"
-											>Remember me</label
-										>
-									</div>
-								</div>
-								<a
-									href="#"
-									class="text-sm font-medium text-white hover:underline dark:text-primary-500"
-									>Forgot password?</a
-								>
-							</div>
+							<p v-if="errMsg" class="text-red-500 mb-4">{{ errMsg }}</p>
 							<button
 								type="submit"
 								class="w-full text-white bg-blue-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
 								Sign in
 							</button>
-							<p class="text-sm font-light text-gray-500 dark:text-gray-400">
-								Don’t have an account yet ?
-								<a
-									href="#"
-									class="font-medium text-white hover:underline dark:text-primary-500">
-									Sign up</a
-								>
-							</p>
 						</form>
 					</div>
 				</div>
